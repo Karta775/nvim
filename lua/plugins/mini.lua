2a8825011fa5b0dbcc0e54d86f03f1a34f5825a7
Karta775 <83785288+Karta775@users.noreply.github.com>
2024-09-02 14:19:17 +0100

    Make mini.icons mock nvim-web-devicons

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,6 +3,15 @@ return {
 		"echasnovski/mini.nvim",
 		lazy = false,
 		version = false,
+    specs = {
+      { "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
+    },
+    init = function()
+      package.preload["nvim-web-devicons"] = function()
+        require("mini.icons").mock_nvim_web_devicons()
+        return package.loaded["nvim-web-devicons"]
+      end
+    end,
 		config = function()
 			require("mini.bracketed").setup()
 			require("mini.comment").setup()
