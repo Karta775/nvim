cf21a76bc63b4f00a2702c96cc8540ca85ff7636
Karta775 <83785288+Karta775@users.noreply.github.com>
2024-09-07 17:15:14 +0100

    Indentation fix

--- lua/plugins/typst.lua
+++ lua/plugins/typst.lua
@@ -1,9 +1,9 @@
 return {
-	'kaarmu/typst.vim',
-	ft = 'typst',
-	lazy=true,
-	config = function ()
-		vim.g.typst_conceal = 1
-		vim.g.typst_auto_close_toc = 1
-	end
+  'kaarmu/typst.vim',
+  ft = 'typst',
+  lazy = true,
+  config = function()
+    vim.g.typst_conceal = 1
+    vim.g.typst_auto_close_toc = 1
+  end
 }
