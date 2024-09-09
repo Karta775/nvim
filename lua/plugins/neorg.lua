c6db344442393390a9822a52a7b42cdc12453aea
Karta775 <83785288+Karta775@users.noreply.github.com>
2024-09-09 15:47:50 +0100

    Add separate workspace for journal and change default workspaces

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,34 +1,49 @@
 return {
-	{
-		"nvim-neorg/neorg",
-		event = "VeryLazy",
-		version = "*",
-		dependencies = {
-			{ "nvim-lua/plenary.nvim" },
-			{ "nvim-neorg/neorg-telescope" },
-		},
-		config = function()
-			require("neorg").setup({
-				load = {
-					["core.defaults"] = {},
-					["core.keybinds"] = {},
-					["core.concealer"] = {},
-					["core.summary"] = {
-						config = {
-							strategy = "by_path"
-						}
-					},
-					["core.integrations.telescope"] = {},
-					["core.dirman"] = {
-						config = {
-							workspaces = {
-								notes = "~/Documents/neorg/notes",
-							},
-							default_workspace = "notes",
-						},
-					},
-				},
-			})
-		end,
-	},
+  {
+    "nvim-neorg/neorg",
+    event = "VeryLazy",
+    version = "*",
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-neorg/neorg-telescope" },
+      { "juniorsundar/neorg-extras" },
+    },
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {},
+          ["core.keybinds"] = {},
+          ["core.concealer"] = {},
+          ["core.summary"] = {
+            config = {
+              strategy = "by_path"
+            }
+          },
+          ["core.integrations.telescope"] = {},
+          ["core.journal"] = {
+            config = {
+              journal_folder = "",
+              workspace = "journal"
+            }
+          },
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                notes = "~/norg/notes",
+                journal = "~/norg/journal",
+              },
+              default_workspace = "notes",
+            },
+          },
+          ["external.agenda"] = {},
+          ["external.roam"] = {},
+          ["external.many-mans"] = {
+            config = {
+              treesitter_fold = true
+            }
+          },
+        },
+      })
+    end,
+  },
 }
