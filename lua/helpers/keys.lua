8d289c9d9d86608d5251935fe0dea80f7a786517
Karta775 <83785288+Karta775@users.noreply.github.com>
2024-09-09 21:27:45 +0100

    Add buf_map to helpers.keys

--- lua/helpers/keys.lua
+++ lua/helpers/keys.lua
@@ -1,24 +1,28 @@
 local M = {}
 
 M.map = function(mode, lhs, rhs, desc)
-	vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc })
+  vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc })
 end
 
 M.hidden_map = function(mode, lhs, rhs, desc)
-	vim.keymap.set(mode, lhs, rhs, { silent = true, hidden = true, desc = desc })
+  vim.keymap.set(mode, lhs, rhs, { silent = true, hidden = true, desc = desc })
 end
 
 M.lsp_map = function(lhs, rhs, bufnr, desc)
-	vim.keymap.set("n", lhs, rhs, { silent = true, buffer = bufnr, desc = desc })
+  vim.keymap.set("n", lhs, rhs, { silent = true, buffer = bufnr, desc = desc })
+end
+
+M.buf_map = function(lhs, rhs, bufnr, desc)
+  vim.keymap.set("n", lhs, rhs, { silent = true, buffer = bufnr, desc = desc })
 end
 
 M.dap_map = function(mode, lhs, rhs, desc)
-	M.map(mode, lhs, rhs, desc)
+  M.map(mode, lhs, rhs, desc)
 end
 
 M.set_leader = function(leader, localleader)
-	vim.g.mapleader = leader
-	vim.g.maplocalleader = localleader
+  vim.g.mapleader = leader
+  vim.g.maplocalleader = localleader
 end
 
 return M
