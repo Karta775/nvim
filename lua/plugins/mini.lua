de8fccb21394650fd79259c8570e08310f5cea0e
Karta775 <83785288+Karta775@users.noreply.github.com>
2024-09-10 23:34:14 +0100

    Remove mini.indentscope
    It's just a touch too much visual clutter. Also it shows up in unwanted
    places like the Dashboard, and working around that would be annoying.

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -38,21 +38,6 @@ return {
         },
       })
 
-      require("mini.indentscope").setup({
-        draw = {
-          delay = 100,
-          animation = require("mini.indentscope").gen_animation.none(),
-        },
-        symbol = "│",
-      })
-
-      -- require("mini.completion").setup()
-      -- local imap_expr = function(lhs, rhs)
-      --   vim.keymap.set("i", lhs, rhs, { expr = true })
-      -- end
-      -- imap_expr("<Tab>", [[pumvisible() ? "\<C-n>" : "\<Tab>"]])
-      -- imap_expr("<S-Tab>", [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]])
-
       local notify = require("mini.notify")
       notify.setup({
         lsp_progress = {
