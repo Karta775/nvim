b0b0d1aa35c1142d7bbecdbffed21854c75032ec
Karta775 <83785288+Karta775@users.noreply.github.com>
2024-09-23 13:17:13 +0100

    Provide better mappings for leap.nvim
    You can now jump back and forth in the results.

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -2,9 +2,20 @@ return {
   'ggandor/leap.nvim',
   event = "VeryLazy",
   config = function()
-    vim.keymap.set('n', 's', '<Plug>(leap)')
-    vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
-    vim.keymap.set({ 'x', 'o' }, 's', '<Plug>(leap-forward)')
-    vim.keymap.set({ 'x', 'o' }, 'S', '<Plug>(leap-backward)')
+    vim.keymap.set('n', '<tab>', '<Plug>(leap)')
+    vim.keymap.set('n', '<s-tab>', '<Plug>(leap-from-window)')
+    vim.keymap.set({ 'x', 'o' }, '<tab>', '<Plug>(leap-forward)')
+    vim.keymap.set({ 'x', 'o' }, '<s-tab>', '<Plug>(leap-backward)')
+
+    local leap = require('leap')
+    leap.opts.special_keys.next_target = '<enter>'
+    leap.opts.special_keys.prev_target = { '<backspace>', '<tab>' }
+    leap.opts.special_keys.next_group = '<space>'
+    leap.opts.special_keys.prev_group = { '<backspace>', '<tab>' }
+
+    require('leap.user').set_repeat_keys('<enter>', '<backspace>', {
+      relative_directions = true,
+      modes = { 'n', 'x', 'o' },
+    })
   end
 }
